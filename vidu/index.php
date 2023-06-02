e079e0552e0be16ff69694f0d8f8fd485b419007
nguyentai1010 <111493850+nguyentai1010@users.noreply.github.com>
2023-06-02 17:14:56 +0700

    php
    php

---
+++ vidu/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            myproject
+        </title>
+    </head>
+    <body>
+    <h1><?php echo 'hello word';
+    ?></h1>
+    </body>
+</html>
